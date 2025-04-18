5c0980ff3f673360bd435cb52a23c99dd4fbfe33
User <User@DESKTOP-REE4GV5>
2025-04-18 23:03:22 +0500

    change

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -11,12 +11,12 @@ void main()
 	const int n = 5;
 	int arr[n] = { 3,5,8 };
 	//arr[1] = 1024;
-	cout << arr[1] << endl;
+	/*cout << arr[1] << endl;
 	for (int i = 0;i < n;i++)
 	{
 		cout << arr[i] << "\t";
 	}
-	cout << endl;
+	cout << endl;*/
 
 #ifdef HOMEWORK
 
@@ -28,16 +28,31 @@ void main()
 	cout << "Введите размер массива ";
 	cin >> SIZE;
 
-	cout << "Введите элемент массива ";
+	cout << "Введите элемент массива " << endl;
 	for (i = 0; i < SIZE;i++)
 	{
-
 		cin >> M[i];
+	}
+
+
+	/*int minRand, maxRand;
+	do
+	{
+		cout << "Введите минимальное рандомное: "; cin >> minRand;
+		cout << "Введите минимальное рандомное: "; cin >> maxRand;
+		if (minRand > maxRand) cout << "Минимальное должно быть меньше максимальногое" << endl;
+		if (minRand == maxRand) cout << "Числа должны быть разные" << endl;
+	} while (minRand >= maxRand);
+	if (maxRand < minRand)
+	{
+		int buffer = minRand;
+		minRand = maxRand;
+		maxRand = buffer;
 	}
 
 	for (i = 0; i < SIZE; i++)
 	{
-		cout << "arr [" << i << "]=" << rand() % (100-50)+50 << endl;
+		cout << "arr [" << i << "]=" << rand() % (maxRand - minRand) + minRand << endl;
 	}
 	cout << endl;
 
@@ -73,6 +88,27 @@ void main()
 		}
 	}
 	cout << "Минимальное значение массива: " << min << endl;
+	*/
+	int Buffer;
+	for (i = 0; i < SIZE; i++)
+	{
+		
+		for (int j = i+1; j < SIZE; j++)
+		{
+			if (M[j] < M[i])
+			{
+				Buffer = M[i];
+				M[i] = M[j];
+				M[j] = Buffer;
+			}
+		}
+
+	}
+	for (int i = 0;i < SIZE;i++)
+	{
+		cout << M[i] << "\t";
+	}
+	cout << endl;
 
 #endif // HOMEWORK
 
